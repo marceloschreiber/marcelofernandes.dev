81be07fc652153821b66fe67b235c1883a3330cf
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-01-06 20:17:42 -0300

    Fix links and change about page
    - Use OutboundLink because of google analytics

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,34 +1,37 @@
 import React from "react"
-
+import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 const Footer = () => (
-    <footer className="bg-black text-gray-300 text-center font-sans tracking-wide text-sm w-full">
-        <div className="my-6 px-3">
-          © {new Date().getFullYear()} Marcelo Fernandes |{" "}
-          <a
-            href="https://github.com/marceloschreiber/marcelofernandes.dev"
-            className="text-blue-600 hover:underline"
-          >
-            Site{" "}
-          </a>
-          built with
-          {` `}
-          <a
-            href="https://www.gatsbyjs.org"
-            className="text-blue-600 hover:underline"
-          >
-            Gatsby
-          </a>{" "}
-          and {" "}
-          <a
-            href="https://tailwindcss.com"
-            className="text-blue-600 hover:underline"
-          >
-            TailwindCSS
-          </a>
-          .
-        </div>
-      </footer>
+  <footer className="bg-black text-gray-300 text-center font-sans tracking-wide text-sm w-full">
+    <div className="my-6 px-3">
+      © {new Date().getFullYear()} Marcelo Fernandes |{" "}
+      <OutboundLink
+        href="https://github.com/marceloschreiber/marcelofernandes.dev"
+        target="_blank"
+        className="text-blue-600 hover:underline"
+      >
+        Site{" "}
+      </OutboundLink>
+      built with
+      {` `}
+      <OutboundLink
+        href="https://www.gatsbyjs.org"
+        target="_blank"
+        className="text-blue-600 hover:underline"
+      >
+        Gatsby
+      </OutboundLink>{" "}
+      and{" "}
+      <OutboundLink
+        href="https://tailwindcss.com"
+        target="_blank"
+        className="text-blue-600 hover:underline"
+      >
+        TailwindCSS
+      </OutboundLink>
+      .
+    </div>
+  </footer>
 )
 
-export default Footer;
+export default Footer
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,30 +12,39 @@ const AboutPage = () => (
       <h1 className="text-4xl font-bold">Hi there,</h1>
       <p className="my-4">
         I'm Marcelo Fernandes, a software developer at{" "}
-        <a
+        <OutboundLink
           href="https://www.sap.com"
+          target="_blank"
           className="font-bold text-blue-700 hover:underline"
         >
           SAP
-        </a>
+        </OutboundLink>
         .
       </p>
       <p className="my-4">
         This is still a work in progress, but my idea is to split the content
         into a{" "}
-        <strong>
-          <Link to="/blog">Blog</Link>
-        </strong>
+        <Link to="/blog" className="font-bold hover:underline">
+          Blog
+        </Link>
         , which will contains longer articles, and a{" "}
-        <strong>
-          <Link to="/today-I-learned">Today I Learned</Link>
-        </strong>{" "}
-        which will contains small things that I learned on my day to day life as
-        a dev.
+        <Link to="/today-I-learned" className="font-bold hover:underline">
+          Today I Learned
+        </Link>{" "}
+        which will contains small things or things that I learned on my day to
+        day life as a dev. Although I might change that in the future, let's see
+        how things go.
       </p>
       <p>
         I also want this to be simple and fast, so I used a set of{" "}
-        <strong>cool technologies</strong> to build this.
+        <OutboundLink
+          href="https://github.com/marceloschreiber/marcelofernandes.dev"
+          target="_blank"
+          className="font-bold text-blue-700 hover:underline"
+        >
+          cool technologies
+        </OutboundLink>{" "}
+        to build this.
       </p>
     </div>
   </Layout>
